288756f98f11bec6d906280a301a6c013b9c9ef0
Chektak <com123com11@naver.com>
2019-04-13 21:53:19 +0900

    4.13 9:53 update

---
+++ Assets/Scripts/UI/DownBTNChangePanel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DownBTNChangePanel : MonoBehaviour {
+
+    public enum PANELSTATE { TRAINING, SHOP, STORY, STORYBOOK, OPTION};
+    public PANELSTATE panelState = PANELSTATE.TRAINING;
+
+    
+}
---
+++ Assets/Scripts/UI/GUIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUIManager : MonoBehaviour {
+    [Header("PANELSTATE의 순서에 따라 패널들을 넣는다.")]
+    public GameObject[] panels;
+    public Button[] buttons;
+    public enum PANELSTATE { TRAINING=0, SHOP, STORY, STORYBOOK, OPTION };
+    public PANELSTATE nowPanelState = PANELSTATE.TRAINING;
+    public void SetNowPanelState(int changeState)
+    {
+        ColorBlock col = buttons[(int)nowPanelState].colors;
+        col.colorMultiplier = 1.5f;
+        buttons[(int)nowPanelState].colors = col;
+        panels[(int)nowPanelState].SetActive(false);
+
+        nowPanelState = (PANELSTATE)changeState;
+
+        col = buttons[(int)nowPanelState].colors;
+        col.colorMultiplier = 1;
+        buttons[(int)nowPanelState].colors = col;
+    }
+}
