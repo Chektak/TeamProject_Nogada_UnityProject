930e31f95984a56aaf9de2c4784dda096b108c48
Chektak <com123com11@naver.com>
2019-04-14 22:22:57 +0900

    4.14 10:20

---
+++ Assets/Scripts/UI/UpgradePanelOnOff.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgradePanelOnOff : MonoBehaviour {
+    public Sprite upStateSprite;
+    public Sprite downStateSprite;
+    public GameObject upStateNogadaButton;
+    public GameObject downStateNogadaButton;
+    public GameObject statUpgradePanel;
+    public float upState_BtnPosY=0;
+    public bool canUp=true;
+
+    private RectTransform rect;
+    private Image image;
+    public void Awake()
+    {
+        rect = GetComponent<RectTransform>();
+        image = GetComponent<Image>();
+    }
+    public void PanelUpDown()
+    {
+        if (canUp == true) {
+            image.sprite = upStateSprite;
+            rect.anchoredPosition3D += new Vector3(0, upState_BtnPosY, 0);
+            statUpgradePanel.SetActive(true);
+            upStateNogadaButton.SetActive(false);
+            downStateNogadaButton.SetActive(true);
+            canUp = false;
+        }
+        else
+        {
+            image.sprite = downStateSprite;
+            rect.anchoredPosition3D -= new Vector3(0, upState_BtnPosY, 0);
+            statUpgradePanel.SetActive(false);
+            upStateNogadaButton.SetActive(true);
+            downStateNogadaButton.SetActive(false);
+            canUp = true;
+        }
+    }
+}
